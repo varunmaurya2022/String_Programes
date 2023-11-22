48ffe6703dcde3fd8c18ddb76186254fa925a5e4
varun maurya <varunmaurya@2022>
2023-11-22 08:46:51 +0530

    add Replace files

--- PositionConvertChar.java
+++ PositionConvertChar.java
@@ -1,14 +1,14 @@
-public class PositionConvertChar{
-	public static void main(String... args){
-		char ch[]={'A','p','p','s','q','u','a','d','z'};
-		
-		String s=new String(ch,2,3);
+public class PositionConvertChar {
+	public static void main(String... args) {
+		char ch[] = { 'A', 'p', 'p', 's', 'q', 'u', 'a', 'd', 'z' };
+
+		String s = new String(ch, 2, 3);
 		System.out.println(s);
-		
-		char chars[]={'H','e','l','l','o'};
-		//Question
-		String str=new String();
-		String string=str.valueOf(chars,2,3);
+
+		char chars[] = { 'H', 'e', 'l', 'l', 'o' };
+		// Question
+		String str = new String();
+		String string = str.valueOf(chars, 2, 3);
 		System.out.println(string);
 	}
 }
---
+++ Replace.java
@@ -0,0 +1,12 @@
+public class Replace {
+   public static void main(String... args) {
+      String str = new String("welcome to Tutorials of String");
+
+      System.out.print("Return Value ");
+      System.out.println(str.replace('o', 't'));
+
+      System.out.print("Return Value ");
+      System.out.println(str.replace('l', 'D'));
+
+   }
+}
